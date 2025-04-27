11a02c3be5c31e09df058fb81fb1b8c5649af6e6
lokoops-tech <kosgeialex939@gmail.com>
2025-04-27 16:32:20 +0300

    Update Item.jsx

--- frontend/src/components/item/Item.jsx
+++ frontend/src/components/item/Item.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import SEO from '../../pages/Seo';
 import './Item.css';
+import DefaultImage from'../../Assets/logo.png';
 
 const Item = (props) => {
     const handleClick = () => {
@@ -44,7 +45,7 @@ const Item = (props) => {
                     src={props.image}
                     alt={props.name}
                     onError={(e) => {
-                        e.target.src = '/path/to/default-image.jpg'; // Add a fallback image
+                        e.target.src ={DefaultImage}; // Add a fallback image
                     }}
                 />
             </Link>
