e37454bb248f25af180f8eb8df734b82eb3b57c0
lokoops-tech <kosgeialex939@gmail.com>
2025-05-05 18:48:22 +0300

    Update ForgetPassword.jsx

--- frontend/src/components/Forgetpassword/ForgetPassword.jsx
+++ frontend/src/components/Forgetpassword/ForgetPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ForgetPassword.css';
-const API_BASE_URL = "http://localhost:4000"; // Adjust this to your API base URL
+const API_BASE_URL = "https://ecommerce-axdj.onrender.com"; // Adjust this to your API base URL
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('');
