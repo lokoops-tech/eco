f0545be09a6b4b65aeaa5fa8b5189afd6e2bdd78
lokoops-tech <kosgeialex939@gmail.com>
2025-05-11 01:05:49 +0300

    Update ForgetPassword.jsx

--- frontend/src/components/Forgetpassword/ForgetPassword.jsx
+++ frontend/src/components/Forgetpassword/ForgetPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ForgetPassword.css';
-const API_BASE_URL = "https://ecommerce-axdj.onrender.com"; // Adjust this to your API base URL
+const API_BASE_URL = "https://gich-backend.onrender.com"; // Adjust this to your API base URL
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('');
